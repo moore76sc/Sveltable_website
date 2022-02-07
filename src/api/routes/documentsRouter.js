7e3f6fe759ec6438f55244419050d0d10d1f6a60
moore76sc <moore76sc@gmail.com>
2022-02-07 15:03:44 -0800

    finished controller and changed path for get one router

--- src/api/routes/documentsRouter.js
+++ src/api/routes/documentsRouter.js
@@ -6,6 +6,6 @@ router.get('/all', documentsController.getAllDocuments,(req, res) => {
   return res.status(201).json({entries: res.locals.documents})
 });
 
-router.get('/:id', documentsController.getOneDocument, (req, res) => {
+router.get('/one', documentsController.getOneDocument, (req, res) => {
   return res.status(201).json({entries: res.locals.oneDocument})
 });
