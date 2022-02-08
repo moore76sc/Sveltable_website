e9626ea8b7151440dbd6721bc90996d81c9ce0a5
TheMimStack <mimi.tk@yahoo.com>
2022-02-08 17:19:03 -0600

    Frontend code

---
+++ src/app/App.svelte
@@ -0,0 +1,28 @@
+<script>
+  import Header from "./Header.svelte";
+  import Main from "./Main.svelte";
+
+  let y;
+</script>
+
+<svelte:window bind:scrollY={y} />
+
+<main>
+  <Header />
+  <div>{y}</div>
+  <Main {y} />
+  <div class="temp" />
+</main>
+
+<style>
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+
+  .temp {
+    background-color: #002940;
+    height: 700px;
+  }
+</style>
