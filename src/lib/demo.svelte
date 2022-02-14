874689286581865dced36ec479c40816af1b8698
moore76sc <moore76sc@gmail.com>
2022-02-14 09:43:20 -0800

    completed api for table

--- src/lib/demo.svelte
+++ src/lib/demo.svelte
@@ -1,41 +1,11 @@
-<script context='module'>
-/* console.log('context fire')
-  export const load = async ({ fetch }) => {
-  console.log('load fire')
-    const res = await fetch('./api');
-    const data = await res.json();
-    console.log(data)
-    return {
-      props:{
-        data,
-      },
-    };
-  }; */
-</script>
- 
+
 <script>
-const getDataSet = async () => {
-  const res = await fetch ("/");
-  const data = await res.text();
-  if (res.ok){
-    console.log(data)
-    return data}
-  else {
-    throw new Error(data);
-    }
-    }
-    let promise = getDataSet();
-
 import SvelTable from 'sveltables'
-
+export let dataSet;
 </script>
 
 <div class="DemoContainer">
-
-  {#await promise then data}
-	<SvelTable dataSet={data}/>
-{/await}
-
+	<SvelTable dataSet={dataSet}/>
 </div>
 
 <style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,9 +1,22 @@
-<script lang="ts">
+<script>
   import Header from '../lib/Header.svelte';
   import Main from '../lib/Main.svelte';
   import Features from '../lib/Features.svelte';
   import Demo from '../lib/demo.svelte';
   // let y;
+  
+  const getDataSet = async () => {
+  const res = await fetch ("/api");//fetches the exported data from index.js. We are not sending a request body
+  const data = await res.json();//parses with json and assigns to data
+  if (res.ok){//if the response body is populated it will return it as props
+    return {
+    props: data
+    }
+  } else {
+    throw new Error(data);
+    }
+    }
+  let promise = getDataSet();
 </script>
 
 <div class="AppContainer">
@@ -13,8 +26,10 @@
   <div class="MainContainer">
     <Main />
   </div>
-  <div class="DemoContainer">
-    <Demo />
+  <div class="DemoContainer"><!-- using await, the demo component won't render until the getDataSet function completes. We passing the response props into Demo -->
+    {#await promise then output}
+      <Demo dataSet={output.props}/>
+    {/await}
   </div>
   <!-- <div class="FeatureContainer">
     <Features />
